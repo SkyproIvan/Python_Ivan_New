4fa94895c380063f18d917ba8933e38b0022e5b2
admin_one <ivan.nik.test.1988@gmail.com>
2026-04-22 05:48:22 +0300

    В ДЗ №6 убрал sleep. В задании 2 добавил явное ожидание

--- 06_lesson/lesson06_task2.py
+++ 06_lesson/lesson06_task2.py
@@ -1,8 +1,9 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
@@ -13,7 +14,8 @@ SkyPro.send_keys("SkyPro")
 
 Old_button = driver.find_element(By.CSS_SELECTOR, "#updatingButton").click()
 
-txt = driver.find_element(By.CSS_SELECTOR, "#updatingButton").text
+txt = WebDriverWait(driver, 20).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "#updatingButton"))).text
 
 print(txt)
 driver.quit()
